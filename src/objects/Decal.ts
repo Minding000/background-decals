cc811631f088072195d3bb1a1606f109e0b2965f
florianmaierd <florianmaierd@gmail.com>
2022-10-03 10:23:53 +0200

    improve: use requestAnimationFrame to render decal, add fps cap

--- src/objects/Decal.ts
+++ src/objects/Decal.ts
@@ -3,16 +3,20 @@ import { Layer } from './Layer';
 export class Decal {
 	private readonly canvas: HTMLCanvasElement
 	private readonly context: WebGL2RenderingContext
-	private interval: number|null = null
+	private isAnimating: boolean = false
+	private lastPerformanceNow = 0
 
 	public constructor(
-		private readonly layers: Layer[]
+		private readonly layers: Layer[],
+		private maxFps: number
 	) {
 		this.canvas = document.createElement('canvas')
 		this.canvas.width = 800
 		this.canvas.height = 600
+		this.maxFps = this.maxFps || 120
 		this.context = this.canvas.getContext("webgl2")
 		this.context.viewport(0, 0, this.canvas.width, this.canvas.height);
+
 		for(const layer of this.layers)
 			layer.setUp(this.context)
 	}
@@ -22,16 +26,35 @@ export class Decal {
 	}
 
 	public start(): void {
-		this.interval = setInterval(() => {
-			for(const layer of this.layers)
-				layer.render()
-		}, 10)
+		this.isAnimating = true
+		requestAnimationFrame(() => this.render())
+	}
+
+	private render(): void {
+		if (!this.isAnimating)
+			return
+
+		requestAnimationFrame(() => this.render())
+
+		if(!this.fpsCapElapsed())
+			return
+
+		this.renderLayers()
+
+		this.lastPerformanceNow = performance.now()
+	}
+
+	private fpsCapElapsed(): boolean {
+		const msElapsed = performance.now() - this.lastPerformanceNow
+		return msElapsed > 1000 / this.maxFps
+	}
+
+	private renderLayers(): void {
+		for(const layer of this.layers)
+			layer.render()
 	}
 
 	public stop(): void {
-		if(this.interval != null) {
-			clearInterval(this.interval)
-			this.interval = null
-		}
+		this.isAnimating = false
 	}
 }
