e74ff07fd8861cde03677b157cf1aa3157bbf350
= <=>
2022-10-05 22:21:30 +0200

    chore: update-coverage script, add c8 dependency

---
+++ scripts/update-coverage.js
@@ -0,0 +1,30 @@
+const fs = require("fs")
+
+const coverageOutput = fs.readFileSync("console-coverage.txt").toString()
+const coverageRegexMatch = coverageOutput.match(/All\sfiles.*?\s+(\d+.\d+)/)
+
+if (coverageRegexMatch) {
+	const coverageFloat = parseFloat(coverageRegexMatch[1])
+	const badgeColor = getBadgeColorByCoverage(coverageFloat)
+	const badgeUrl = getBadgeCoverageUrl("coverage", coverageFloat, badgeColor)
+	// push to gist
+	console.log(badgeUrl)
+}
+
+function getBadgeCoverageUrl(title, coverage, color) {
+	// shields coverage url will look like this:
+	// https://shields.io/badge/[TEXT_LEFT]-[TEXT-RIGHT]-[COLOR-RIGHT]
+	const shieldsCoverageUrl = "https://shields.io/badge/"
+	return `${shieldsCoverageUrl}${title}-${coverage}-${color}`
+}
+
+function getBadgeColorByCoverage(coverageNumber) {
+	if (coverageNumber < 50)
+		return "red"
+	if (coverageNumber < 75)
+		return "orange"
+	if (coverageNumber < 90)
+		return "yellow"
+	else
+		return "brightgreen"
+}
